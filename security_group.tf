2e76f5fbbf84e7cc6020b2a644b5e5d3d5a57036
Michael <ulm@acai.gmbh>
2021-01-01 21:07:31 +0100

    initial commit

---
+++ security_group.tf
@@ -0,0 +1,37 @@
+locals {
+    vpc_id = ""
+}
+
+resource "aws_security_group" "demo_sg" {
+  name        = "acai-lab-demo-sg"
+  description = "This security group will toggle between open and restricted"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  egress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+}
